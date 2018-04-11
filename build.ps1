3d4984fa9fc2de4de9708b488c67dba7f4a34e30
Matthew Casperson <matthewcasperson@gmail.com>
2018-04-11 13:09:44 +1000

    Still attempting to remove the top level directory

--- build.ps1
+++ build.ps1
@@ -6,8 +6,10 @@ $gcpSdkUrl = "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-
 $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -UseBasicParsing -Uri $gcpSdkUrl -OutFile google-cloud-sdk.zip
 Remove-Item "google-cloud-sdk" -Recurse -ErrorAction Ignore
+Remove-Item "win" -Recurse -ErrorAction Ignore
 $cwd = (Get-Item -Path ".\" -Verbose).FullName
 [System.IO.Compression.ZipFile]::ExtractToDirectory("$cwd\google-cloud-sdk.zip", $cwd)
+Rename-Item -Path google-cloud-sdk -NewName win
 
 $gcpSdkUrl -match "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-(\d+\.+\d+\.\d+)"
 $version = $Matches[1]
